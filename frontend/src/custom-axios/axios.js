7d2108c028002136ed43bfe3921f2d7cd73c64bd
draganageorgievska <73995028+draganageorgievska@users.noreply.github.com>
2022-04-08 21:29:46 +0200

    Update axios.js

--- frontend/src/custom-axios/axios.js
+++ frontend/src/custom-axios/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance=axios.create({
-    baseURL:'http://localhost:8080/api',
+    baseURL:'https://spring-boot-181534.herokuapp.com/api',
     headers:{
         'Access-Control-Allow-Origin': '*'
     }
